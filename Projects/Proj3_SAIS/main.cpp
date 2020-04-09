316c2419d04482637b39e790855adb3246149953
subhed <sachavan@mail.csuchico.edu>
2020-04-09 00:44:46 -0700

    Project 3 Completed

--- Projects/Proj3_SAIS/main.cpp
+++ Projects/Proj3_SAIS/main.cpp
@@ -45,19 +45,12 @@ int main()
     }
     Tint.push_back(0);
 
-    // for (size_t i = 0; i < Tint.size(); i++)
-    // {
-    //     cout << Tint[i] << " ";
-    // }
-    // cout << endl;
-
-    // Vector t - Type Array
-
     vector<int> t(Tint.size());
 
     t[Tint.size() - 1] = 1;
 
-    for (size_t i = Tint.size() - 2; i > 0; --i)
+    // for (size_t i = Tint.size() - 2; i > 0; --i)
+    for (unsigned i = Tint.size() - 1; i-- > 0;)
     {
         if (Tint[i] > Tint[i + 1])
         {
@@ -73,12 +66,6 @@ int main()
         }
     }
 
-    // for (size_t i = 0; i < t.size(); i++)
-    // {
-    //     cout << t[i] << " ";
-    // }
-    // cout << endl;
-
     vector<int> lms(Tint.size());
 
     for (size_t i = Tint.size() - 1; i > 0; --i)
@@ -93,50 +80,32 @@ int main()
         }
     }
 
-    // for (size_t i = 0; i < lms.size(); i++)
-    // {
-    //     cout <<lms[i] << " ";
-    // }
-    // cout << endl;
-
     // Max Element in Array
 
     int maxi = *max_element(Tint.begin(), Tint.end());
-    // cout << maxi;
-    // cout << endl;
+    int size = maxi + 1;
+
+    // cout<<maxi<<endl;
 
     // Vector A
 
-    vector<int> A(maxi + 1, 0);
+    vector<int> A(size, 0);
 
     for (size_t i = 0; i < Tint.size(); i++)
     {
         A[Tint[i]] = A[Tint[i]] + 1;
     }
 
-    // for (size_t i = 0; i < A.size(); i++)
-    // {
-    //     cout << A[i] << " ";
-    // }
-    // cout << endl;
-
     // Vector C
-    vector<int> BC(maxi + 1, 0);
-    vector<int> BE(maxi + 1, 0);
+    vector<int> BC(size);
+    vector<int> BE(size);
 
-    vector<int> C(maxi + 1, 0);
-    C[0] = 0;
+    vector<int> C(size, 0);
 
-    for (size_t i = 1; i < Tint.size(); i++)
+    for (size_t i = 1; i < A.size(); i++)
     {
         C[i] = C[i - 1] + A[i - 1];
     }
-
-    // for (size_t i = 0; i < C.size(); i++)
-    // {
-    //     cout << C[i] << " ";
-    // }
-    // cout << endl;
 
     // Vector End
 
@@ -148,81 +117,70 @@ int main()
         End[i - 1] = C[i] - 1;
     }
 
-    // for (size_t i = 0; i < C.size(); i++)
-    // {
-    //     cout << End[i] << " ";
-    // }
-    // cout << endl;
-
     BC = C;
     BE = End;
 
     vector<int> SA(Tint.size(), -1);
-    
-    for (size_t i = lms.size()-1; i > 0; --i)
+
+    for (size_t i = lms.size() - 1; i > 0; --i)
     {
-        if(lms[i]==1){
+        if (lms[i] == 1)
+        {
             SA[End[Tint[i]]] = i;
-            End[Tint[i]] = End[Tint[i]] - 1; 
+            End[Tint[i]] = End[Tint[i]] - 1;
         }
     }
 
-
     for (size_t i = 0; i < SA.size(); i++)
     {
-        if(SA[i]==-1){
+        if (SA[i] == -1)
+        {
             continue;
         }
-        else if(SA[i]==0){
+        else if (SA[i] == 0)
+        {
             continue;
         }
         else
         {
             int x = SA[i] - 1;
 
-            if(t[x] == 0){
+            if (t[x] == 0)
+            {
                 SA[C[Tint[x]]] = x;
-                C[Tint[x]] = C[Tint[x]]+1;
+                C[Tint[x]] = C[Tint[x]] + 1;
             }
-
         }
-        
     }
- 
 
-    
-
-    for (unsigned i = SA.size(); i-- > 0; )
+    for (unsigned i = SA.size(); i-- > 0;)
     {
-         if(SA[i]==-1){
+        if (SA[i] == -1)
+        {
             continue;
         }
-        if(SA[i]==0){
+        if (SA[i] == 0)
+        {
             continue;
         }
         else
         {
 
-        int x = SA[i]-1;
+            int x = SA[i] - 1;
 
-        if(t[x]==1){
-            SA[BE[Tint[x]]] = x;
-            BE[Tint[x]] = BE[Tint[x]] - 1;
+            if (t[x] == 1)
+            {
+                SA[BE[Tint[x]]] = x;
+                BE[Tint[x]] = BE[Tint[x]] - 1;
+            }
         }
-
-        }
-        
-       
     }
-
 
     for (size_t i = 0; i < SA.size(); i++)
     {
-        cout<< SA[i]<<" ";
+        cout << SA[i] << " ";
     }
-    cout<<endl;    
-
-
+    cout << endl;
 
     return 0;
 }
